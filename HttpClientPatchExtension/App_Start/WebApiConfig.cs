052df863c5b3ce1c17d2608f0f83ccbfada45174
Adnan Mulalic <stensolino@gmail.com>
2017-05-14 17:18:39 +0200

    Remove XML Formatter

--- HttpClientPatchExtension/App_Start/WebApiConfig.cs
+++ HttpClientPatchExtension/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +12,9 @@ namespace HttpClientPatchExtension
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
